2363db559cfef8b3edaaeac86aad270fc86906e1
sabinala <sabina.altus@pnnl.gov>
2023-07-12 07:09:33 -0700

    more on ensemble scenario

--- src/pyciemss/utils/hackathon_scenario2_utils/hackathon12mo_ensemble_challenge_utils.py
+++ src/pyciemss/utils/hackathon_scenario2_utils/hackathon12mo_ensemble_challenge_utils.py
@@ -1,12 +1,7 @@
 import pandas as pd
 import numpy as np
 
-url = 'https://github.com/reichlab/covid19-forecast-hub/blob/master/data-truth/truth-Incident%20Cases.csv'
-raw_cases = pd.read_csv(url)
-url = 'https://media.githubusercontent.com/media/reichlab/covid19-forecast-hub/master/data-truth/truth-Incident%20Hospitalizations.csv'
-raw_hosp = pd.read_csv(url)
-url = 'https://media.githubusercontent.com/media/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv'
-raw_deaths = pd.read_csv(url)
+
 
 def get_state_data(state_fips):
 
