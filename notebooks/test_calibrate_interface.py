137a2740bac10209c63869251700b8c12a1ad824
liunelson <nliu@uncharted.software>
2024-06-26 02:46:33 +0000

    Calibrate charts

--- notebooks/test_calibrate_interface.py
+++ notebooks/test_calibrate_interface.py
@@ -12,11 +12,14 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 # from typing import Dict, List, Callable
 
 import pyciemss.visuals.plots as plots
 import pyciemss.visuals.vega as vega
 import pyciemss.visuals.trajectories as trajectories
+
+from pyciemss.integration_utils.observation import load_data
 
 from pyciemss.integration_utils.intervention_builder import (
     param_value_objective,
@@ -35,20 +38,35 @@ model3 = os.path.join(MODEL_PATH, "SIR_stockflow.json")
 dataset1 = os.path.join(DATA_PATH, "SIR_data_case_hosp.csv")
 dataset2 = os.path.join(DATA_PATH, "traditional.csv")
 
+d = load_data(dataset1)
+d = {"timepoint": d[0]} | d[1]
+d = {k: v.numpy() for k, v in d.items()}
+dataset1_df = pd.DataFrame(d)
+
 # %%
+# Settings
+
 start_time = 0.0
-end_time = 100.0
-logging_step_size = 10.0
+end_time = 5.0
+logging_step_size = 0.1
 num_samples = 100
 
 # %%
+# Run pre-calibration Simulate
+
+result_precalibrate = pyciemss.sample(model1, end_time, logging_step_size, num_samples, start_time=start_time)
+
+result_precalibrate["data"].head()
+
+# %%
+# Run Calibrate
+
 data_mapping = {"case": "infected", "hosp": "hospitalized"} # data is mapped to observables
 # data_mapping = {"case": "I", "hosp": "H"} # data is mapped to state variables
 
 num_iterations = 10
 
-# Calibrate model1 with dataset1
-calibrated_results = pyciemss.calibrate(
+result_calibrate = pyciemss.calibrate(
     model1, 
     dataset1, 
     data_mapping = data_mapping, 
@@ -56,15 +74,15 @@ calibrated_results = pyciemss.calibrate(
 )
 
 # %%
-calibrated_results
+result_calibrate
 
-# calibrated_results = {
+# result_calibrate = {
 #     "inferred_parameters": AutoGuideList((0): AutoDelta(), (1): AutoLowRankMultivariateNormal())
 #     "loss": <float>y
 # }
 
 # %%
-parameter_estimates = calibrated_results["inferred_parameters"]
+parameter_estimates = result_calibrate["inferred_parameters"]
 parameter_estimates()
 
 # parameter_estimates() = {
@@ -77,45 +95,113 @@ parameter_estimates()
 # }
 
 # %%
-# Sample from the parameter estimates and simulate model1 using the sampled configurations
-calibrated_sample_results = pyciemss.sample(
+# Run post-calibration Simulate
+result_postcalibrate = pyciemss.sample(
     model1, 
     end_time, 
     logging_step_size, 
     num_samples,
     start_time = start_time, 
-    inferred_parameters = parameter_estimates
+    inferred_parameters = result_calibrate["inferred_parameters"]
 )
 
-calibrated_sample_results["data"].head()
+result_postcalibrate["data"].head()
 
 # %%
-data0 = calibrated_sample_results["data"][calibrated_sample_results["data"]["timepoint_id"] == 0]
+# Plot trajectories
+
+cmap = mpl.colormaps["tab10"](range(10))
+fig, axes = plt.subplots(nrows = 2, ncols = 1, figsize = (8, 8))
+fig.suptitle("State Variable Trajectories")
+fig.tight_layout()
+
+for ax, (train_data_state, model_state) in zip(axes, data_mapping.items()):
+
+    # Pre-/post-calibrate trajectories
+    for result_, label, color in zip((result_precalibrate, result_postcalibrate), ("Pre-Calibration", "Post-Calibration"), (cmap[0, :], cmap[1, :])):
+
+        x = result_["data"][result_["data"]["sample_id"] == 0]["timepoint_unknown"]
+        for i in range(num_samples):
+            result = result_["data"][result_["data"]["sample_id"] == i]
+            
+            if "_".join([model_state, "state"]) in result.columns:
+                l = "_".join([model_state, "state"])
+                y = result[l]
+            elif "_".join([model_state, "observable", "state"]) in result.columns:
+                l = "_".join([model_state, "observable", "state"])
+                y = result[l]
+            else:
+                l = None
+                y = None
+            
+            if ~isinstance(y, type(None)):
+                ax.plot(x, y, linewidth = 0.5, color = color, alpha = 0.8, label = label)
+
+
+    # Training dataset trajectories
+    ax.plot(dataset1_df["timepoint"], dataset1_df[train_data_state], marker = "o", color = "black", markeredgecolor = "black", markerfacecolor= "white", label = train_data_state)      
+    
+    # Axis labels
+    __ = plt.setp(ax, xlabel = "Time (Days)", ylabel = model_state)
+
+    # Legend
+    l = [
+        mpl.lines.Line2D([0], [0], label = "Observations", marker = "o", color = "black", markeredgecolor = "black", markerfacecolor= "white"),
+        mpl.lines.Line2D([0], [0], label = "Pre-Calibration", linewidth = 0.5, color = cmap[0, :], alpha = 0.8),
+        mpl.lines.Line2D([0], [0], label = "Post-Calibration", linewidth = 0.5, color = cmap[1, :], alpha = 0.8)
+    ]
+    __ = ax.legend(handles = l)
+
+
+fig.savefig("./figures/test_calibrate_interface_state_trajectories.png")
+
+# %%
+# Plot parameter distributions
 
 parameters = list(parameter_estimates().keys())
-parameter_samples = {p: data0[p + "_param"] for p in parameter_estimates().keys()}
+num_parameters = len(parameters)
 
-# %%
-# Plot sampled configuration
-fig, axes = plt.subplots(nrows = 6, ncols = 1, figsize = (8, 12))
+fig, axes = plt.subplots(nrows = num_parameters, ncols = 1, figsize = (8, 15))
+fig.suptitle("Parameter Distributions")
 fig.tight_layout()
 
 for ax, p in zip(axes, parameters):
     
-    __ = ax.hist(parameter_samples[p], density = False, label = p)
 
-    m = np.nanmean(parameter_samples[p])
-    s = np.nanstd(parameter_samples[p])
-    v = np.var(parameter_samples[p])
+    # Pre-/post-calibrate trajectories
+    for i, (result_, label, color) in enumerate(zip((result_precalibrate, result_postcalibrate), ("Prior", "Posterior"), (cmap[0, :], cmap[1, :]))):
 
-    __ = ax.plot([m, m], [0, 2 * np.sqrt(num_samples)], linewidth = 2)
-    __ = ax.errorbar(m, np.sqrt(num_samples), yerr = None, xerr = s, capsize = 10.0, elinewidth = 2)
+        result = result_["data"][result_["data"]["timepoint_id"] == 0]
+        samples = result[p + "_param"]
+        
 
-    __ = ax.text(m + 1.5 * s, np.sqrt(num_samples), f"mean = {m:.3f}\nstd = {s:.3f}\nvar = {v:.3f}")
+        if i == 0:
+            h, b = np.histogram(samples, density = False)
+            w = b[1] - b[0]
+        else:
+            h, b = np.histogram(samples, density = False, bins = b)
 
-    __ = plt.setp(ax, xlabel = p, ylabel = "Count")
 
-fig.savefig("./figures/test_calibrate_interface.png")
+        x = 0.5 * (b[1:] + b[0:-1])
+        if i == 0:
+            __ = ax.bar(x, h + 0.1, width = 0.9 * w, align = "center", label = label, alpha = 0.7)
+        else:
+            __ = ax.bar(x, h + 0.1, width = 0.5 * w, align = "center", label = label, alpha = 0.7)
+
+
+    # Legend
+    l = [
+        mpl.patches.Patch(facecolor = cmap[0, :], alpha = 0.7, label = "Prior"),
+        mpl.patches.Patch(facecolor = cmap[1, :], alpha = 0.7, label = "Posterior"),
+    ]
+    __ = ax.legend(handles = l)
+
+    # Axis labels
+    xlabel = "_".join(p.split("_")[1:])
+    __ = plt.setp(ax, xlabel = xlabel, ylabel = "Count")
+
+
+fig.savefig("./figures/test_calibrate_interface_parameter_distributions.png")
 
 # %%
 
