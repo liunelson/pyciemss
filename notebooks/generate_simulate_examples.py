5f31eae2352aeddc2a32ed27886457fb78f13d63
Nelson Liu <nliu@uncharted.software>
2025-01-13 17:18:32 -0500

    Testing sensitivity analysis

---
+++ notebooks/generate_simulate_examples.py
@@ -0,0 +1,201 @@
+# %%[markdown]
+# # Test simulate Interface
+#
+# Run Simulate with a simple model
+
+# %%
+import os
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import requests
+
+import pyciemss
+from mira.metamodel import *
+from mira.sources.amr.petrinet import template_model_from_amr_json
+
+# %%
+# Define model A (SIR)
+
+
+# %%
+# Define concepts
+concepts = {
+    'S': Concept(name="S", identifiers={"ido": "0000514"}),
+    'I': Concept(name="I", identifiers={"ido": "0000511"}),
+    'R': Concept(name="R", identifiers={"ido": "0000592"}),
+    'Sus': Concept(name="Sus", identifiers={"ido": "0000514"}),
+    'Inf': Concept(name="Inf", identifiers={"ido": "0000511"}),
+    'Rec': Concept(name="Rec", identifiers={"ido": "0000592"})
+}
+
+# %%
+modelA = TemplateModel(
+    templates = [
+        ControlledConversion(
+            name = 'Infection',
+            subject = concepts['S'],
+            outcome = concepts['I'],
+            controller = concepts['I']
+        ),
+        NaturalConversion(
+            name = 'Recovery',
+            subject = concepts['I'],
+            outcome = concepts['R']
+        )
+    ],
+    parameters = {
+        'b': Parameter(name = 'b', description = 'Infection rate', value = 0.10),
+        'g': Parameter(name = 'g', description = 'Recovery rate', value = 0.05)
+    },
+    initials = {
+        'S': Initial(concept = concepts['S'], expression = SympyExprStr('0.99')),
+        'I': Initial(concept = concepts['I'], expression = SympyExprStr('0.01')),
+        'R': Initial(concept = concepts['R'], expression = SympyExprStr('0.0'))
+    },
+    annotations = Annotations(name = 'Model A', description = 'SIR model')
+)
+
+modelA.templates[0].set_mass_action_rate_law('b')
+modelA.templates[1].set_mass_action_rate_law('g')
+
+modelA.draw_jupyter()
+
+# %%
+# Same SIR model with different configuration
+modelB = TemplateModel(
+    templates = [
+        ControlledConversion(
+            name = 'Infection',
+            subject = concepts['S'],
+            outcome = concepts['I'],
+            controller = concepts['I']
+        ),
+        NaturalConversion(
+            name = 'Recovery',
+            subject = concepts['I'],
+            outcome = concepts['R']
+        )
+    ],
+    parameters = {
+        'b': Parameter(name = 'b', description = 'Infection rate', value = 1.0),
+        'g': Parameter(name = 'g', description = 'Recovery rate', value = 1.0)
+    },
+    initials = {
+        'S': Initial(concept = concepts['S'], expression = SympyExprStr('0.99')),
+        'I': Initial(concept = concepts['I'], expression = SympyExprStr('0.01')),
+        'R': Initial(concept = concepts['R'], expression = SympyExprStr('0.0'))
+    },
+    annotations = Annotations(name = 'Model A', description = 'SIR model')
+)
+
+modelA.templates[0].set_mass_action_rate_law('b')
+modelA.templates[1].set_mass_action_rate_law('g')
+
+modelA.draw_jupyter()
+
+
+
+
+
+
+
+
+
+
+# %%
+
+
+# %%
+# Get test model (basic SIR)
+PATH = "./data"
+model_url = "https://raw.githubusercontent.com/DARPA-ASKEM/simulation-integration/main/data/models/SIR_param_in_observables.json"
+r = requests.get(model_url)
+if r.ok:
+    model = r.json()
+
+# %%
+# Modify the model configuration 
+# by giving priors to parameter values
+
+# beta
+model["semantics"]["ode"]["parameters"][0] = {
+    'id': 'beta',
+    'value': 0.0025,
+    'distribution': {
+        'type': 'StandardUniform1',
+        'parameters': {
+            'minimum': 0.001, 
+            'maximum': 0.003
+        }
+    }
+}
+
+# gamma
+model["semantics"]["ode"]["parameters"][1] = {
+    'id': 'gamma',
+    'value': 0.07,
+    'distribution': {
+        'type': 'StandardUniform1',
+        'parameters': {
+            'minimum': 0.04,
+            'maximum': 0.15
+        }
+    },
+    'units': {
+        'expression': '1/day',
+        'expression_mathml': '<apply><power/><ci>day</ci><cn>-1</cn></apply>'
+    }
+}
+
+# Save modified model
+with open(os.path.join(PATH, "test_simulate_model.json"), "w") as fp:
+    json.dump(model, fp, indent = 4)
+
+
+# %%
+# Simulate
+start_time = 0.0
+end_time = 100.0
+logging_step_size = 1.0
+num_samples = 10
+
+results = pyciemss.sample(
+    model, 
+    end_time, 
+    logging_step_size, 
+    num_samples, 
+    start_time = start_time
+)
+
+# %%
+results["data"].head()
+
+# %%
+# Plot results
+
+colors = mpl.colormaps["tab10"](range(10))
+
+df = results["data"].groupby(["timepoint_id"]).mean()
+
+fig, ax = plt.subplots(1, 1, figsize = (8, 6))
+
+i = 0
+names = []
+for c in results["data"].columns:
+    if c.split("_")[-1] == "state":
+        for n in range(num_samples):
+            df = results["data"][results["data"]["sample_id"] == n]
+            __ = ax.plot(df["timepoint_unknown"], df[c], label = c, alpha = 0.5, color = colors[i, :])
+        
+        names.append(c)
+        i += 1
+
+__ = ax.legend(handles = [mpl.lines.Line2D([0], [0], alpha = 0.5, color = colors[j, :], label = names[j]) for j in range(i)])
+
+# %%
+# Save results["data"] as CSV file
+results["data"].to_csv("./data/test_simulate_interface.csv")
+
+# %%
